7f2f737d0af1dc7a2dc44cbc4886374d0a95e6fd
IdrisVeli1987 <idrisveliev@gmail.com>
2024-03-21 21:35:21 +0400

    add task2 and files

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Task1 Fayldan yazıları oxuyun, uppercase edib digər fayla yazın
 
+// import { readFileSync, writeFileSync } from "fs";
+
+// const dataContent = readFileSync("./node.txt").toString().toUpperCase();
+
+// // console.log(dataContent);
+
+// writeFileSync("node2.txt", dataContent);
+
+// -------------------------------------------------------------------------------
+
+// Task2 Funksiya yaradın. Belə ki, onun işi kopyalamaq olsun. 2 dənə string alsın, faylın adları. Birinci faylı kopyalasın ikinci fayl yaratsın.
+
 import { readFileSync, writeFileSync } from "fs";
 
-const dataContent = readFileSync("./node.txt").toString().toUpperCase();
-
-// console.log(dataContent);
-
-writeFileSync("node2.txt", dataContent);
+function copyFile(from, to) {
+  const fromContent = readFileSync(from);
+  writeFileSync(to, fromContent);
+}
+copyFile('data.txt', 'test.txt')
